3f9d9e235662f54080bfe6abfbd6f66a17cccc8d
b0bogkev <b0bogkev@vasvari.org>
2023-09-25 08:36:22 +0200

    warrior mego

--- WarriorProject/Program.cs
+++ WarriorProject/Program.cs
@@ -3,14 +3,68 @@
  */
 
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace WarriorProject
 {
+    public class Position
+    { 
+        private int x; private int y;
+        public int X { get { return x; } set { x = value; } }
+        public int Y { get { return y; } set { y = value; } }
+
+
+        public Position(int x, int y) { this.x = x; this.y = y;}
+
+        public void ToConsole()
+        {
+            Console.WriteLine($"({x},{y})");
+        }
+    }
+
+    public class Warrior
+    {
+        public string name;
+        public Position position;
+        public Warrior(string name, Position position)
+        {
+            this.name = name;
+            this.position = position;
+            
+        }
+        
+
+
+        public void MoveHorizontal(int posChange)
+        {
+            
+            position.X += posChange;
+      
+        }
+        public void MoveVertical(int posChange)
+        {
+            position.Y += posChange;
+
+        }
+
+
+        public void ToConsole() 
+        {
+            Console.WriteLine($"{name}, position ({position.X},{position.Y})");
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Position position = new Position(2, -1);
+            position.ToConsole();
+            Warrior warrior = new Warrior("Peter the Iron Man", position);
+            warrior.ToConsole();
+            warrior.MoveHorizontal(3);
+            warrior.MoveVertical(-2);
+            warrior.ToConsole();
         }
     }
 }
